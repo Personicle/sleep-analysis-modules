28960e2825b16d1a90612bd83b12b71bd9349fa9
Narendra Badam <nbadam@clearsense.com>
2022-10-03 12:29:43 -0400

    Oct3 activity_sleep_scatterplot

---
+++ activity_sleep_scatterplot.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  3 12:19:18 2022
+
+@author: nbadam
+"""
+
+import numpy as np
+import pandas as pd
+from base_schema import *
+from db_connection import *
+from sqlalchemy import select
+import pandas.io.sql as sqlio
+import json
+
+import sqlite3 as sq3
+from sqlite3 import InterfaceError
+
+from personicle_functions import *
+
+query_events=query_events='select * from  personal_events '
+events_stream= sqlio.read_sql_query(query_events,engine)
+
+data_stream='select * from  calories_total '
+data_stream= sqlio.read_sql_query(data_stream,engine)
+
+data_stream=timestamp_modify(data_stream).copy()
+events_stream=events_overlap(events_stream)
+
+events_stream['duration']=(events_stream['end_time'] - events_stream['start_time']) / pd.Timedelta(hours=1)
+events_stream['end_date'] = pd.to_datetime(events_stream['end_time']).dt.date
+events_stream['end_date']=events_stream['end_date'].astype(str)
+
+
+# Getting list of dates that have high intense activities
+# to be removed to check Step count effect
+
+dates_tobe_excluded=events_stream[events_stream.event_name.isin(['Running','Biking','Strength training','Training','FunctionalStrengthTraining','Rowing','activity','Afternoon Ride','Cycling','Evening Run','Afternoon Run'])]
+
+
+events_stream=events_stream[~events_stream.end_date.isin(dates_tobe_excluded.end_date.unique())].copy()
+
+# Matching events with sleep data
+es1=data_stream.copy()
+es2=events_stream[(events_stream.event_name.isin(['Sleep']))&(events_stream.duration<=15)] #sleep
+
+es1['interval_start'] = es1['start_time'] + timedelta(hours=0)
+es1['interval_end'] = es1['start_time'] + timedelta(hours=24)
+es1['event_name']=es1['unit'].map(eventname_unit)
+
+
+try:
+    es1['parameter2'] = es1['parameter2'].apply(lambda x: json.dumps(x))
+    es2['parameter2'] = es2['parameter2'].apply(lambda x: json.dumps(x))
+
+except KeyError:
+    pass
+    
+# convert datetime to datetime string for sqlite
+for f in ['start_time', 'end_time', 'timestamp', 'interval_start', 'interval_end']:
+    if f in es1.columns:
+        es1[f] = es1[f].apply(lambda x: x.strftime("%Y-%m-%d %H:%M:%S %z"))
+    if f in es2.columns:
+        es2[f] = es2[f].apply(lambda x: x.strftime("%Y-%m-%d %H:%M:%S %z"))
+        
+
+# convert the dfs to in-memory sqlite tables, join the tables, then read as df
+conn = sq3.connect(':memory:')
+# #write the tables
+try:
+    es1.to_sql('es1_name', conn, index=False)
+    es2.to_sql('es2_name', conn, index=False)
+except InterfaceError as e:
+    print("Eventstream 1")
+    print(es1.head())
+    print(es1.dtypes)
+
+    print("Eventstream 2")
+    print(es2.head())
+    print(es2.dtypes)
+    raise e
+
+#es1=steps(datastream), es2=sleep
+es1='es1_name'
+es2='es2_name'
+
+qry = f"""
+    select  
+        {es1}.user_ID,
+        {es1}.event_name activity_name,
+        {es1}.start_time activity_start_time,
+        {es1}.end_time activity_end_time,
+         {es1}.value count,
+        {es2}.event_name effect_event_name,
+        {es2}.start_time sleep_start_time,
+        {es2}.end_time sleep_end_time,
+        {es2}.parameter2 {es2}_parameter2
+        
+        
+
+    from
+        {es2} left join {es1} on
+        (
+        (
+        {es1}.user_id={es2}.user_id
+        )
+        and
+        
+        (
+        ({es2}.start_time between {es1}.interval_start and {es1}.interval_end)
+        
+        or 
+        
+        ({es2}.end_time between {es1}.interval_start and {es1}.interval_end)
+        )
+        )
+
+        
+        
+        
+        
+    """
+
+sleep_event_matched_data = pd.read_sql_query(qry, conn)
+
+
+for col in ['activity_name','activity_start_time','activity_end_time','sleep_start_time', 'sleep_end_time']:
+    sleep_event_matched_data[col].fillna(0,inplace=True)
+    
+
+for f in ['activity_start_time','activity_end_time','sleep_start_time', 'sleep_end_time']:
+    sleep_event_matched_data[f] = pd.to_datetime(sleep_event_matched_data[f], infer_datetime_format=True)
+
+
+sleep_event_matched_data['activity_duration'] =sleep_event_matched_data['activity_end_time'] - sleep_event_matched_data['activity_start_time']
+sleep_event_matched_data['activity_duration']=sleep_event_matched_data['activity_duration']/np.timedelta64(1,'m')
+
+sleep_event_matched_data['sleep_duration'] =sleep_event_matched_data['sleep_end_time'] - sleep_event_matched_data['sleep_start_time']
+sleep_event_matched_data['sleep_duration']=sleep_event_matched_data['sleep_duration']/np.timedelta64(1,'h')
+
+
+sleep_event_matched_data=sleep_event_matched_data[(sleep_event_matched_data.activity_name!=0)].copy()
+
+
+
+if sleep_event_matched_data.activity_name.unique()=='steps': #will change to list soon
+    pivot_sleep=sleep_event_matched_data.pivot_table(index=['user_id','sleep_duration'],columns=['activity_name'],values=['activity_duration','count'],aggfunc=np.sum).fillna(0).reset_index()
+  
+else:
+    pivot_sleep=sleep_event_matched_data.pivot_table(index=['user_id','sleep_duration'],columns=['activity_name'],values=['activity_duration','count'],aggfunc=np.mean).fillna(0).reset_index()
+
+    
+    
+
+new_cols=[('{1} {0}'.format(*tup)) for tup in pivot_sleep.columns]
+
+# assign it to the dataframe (assuming you named it pivoted
+pivot_sleep.columns= new_cols
+
+# resort the index, so you get the columns in the order you specified
+#pivot_sleep.sort_index(axis='columns').head()
+
+
+pivot_sleep.columns = pivot_sleep.columns.str.strip()
+
+pivot_sleep.columns = pivot_sleep.columns. str. replace(' ','').str. replace('activity_duration','')
+
+pivot_sleep.rename(columns={ pivot_sleep.columns[-1]: "activity_value",pivot_sleep.columns[-2]: "activity_duration" },inplace=True)
+
+
+pivot_sleep['activity_name']=sleep_event_matched_data.activity_name.unique()[0]
+pivot_sleep['activity_units']=list(eventname_unit.keys())[list(eventname_unit.values()).index(sleep_event_matched_data.activity_name.unique()[0])]
+
+pivot_sleep.columns = map(str.lower,pivot_sleep.columns)
+
+
+scatterplot_insights=pivot_sleep.copy()
+
+
+dic = {}
+
+
+for user_id in scatterplot_insights.user_id.unique():
+    user_df = scatterplot_insights[scatterplot_insights['user_id']==user_id]
+    l = []
+    for i in range(user_df.shape[0]):
+        row = user_df.iloc[i,:]
+        #print(row) 
+        l.append([row['activity_value'],row['sleep_duration']])
+        
+
+    dic[user_id] = {'XAxis' : {'Measure':scatterplot_insights.activity_name.unique()[0] , 'unit': "Total"+" "+eventname_unit[scatterplot_insights.activity_units.unique()[0]]+" "+"per day"}, 'YAxis': {
+    'Measure': "Sleep",
+    'unit': "hours"
+    }, 'data' : l}
+ 
+
+scatterplot_data = pd.DataFrame(dic.items())
+scatterplot_data.rename(columns={0:'user_id',1:'correlation_result'},inplace=True)
+
+scatterplot_data['analysis_id']= scatterplot_insights['activity_name'].map(activity_analysisid)   # Will be automated based on the analysis
+scatterplot_data['timestampadded']=strftime("%Y-%m-%d %H:%M:%S", gmtime())
+scatterplot_data['view']='No'
+
---
+++ personicle_functions.py
@@ -0,0 +1,277 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 10:33:59 2022
+
+@author: nbadam
+"""
+
+#Activity dictionaries that needs to be updated over time
+
+import numpy as np
+import pandas as pd
+import json
+import math
+from time import gmtime, strftime
+from datetime import datetime
+from datetime import timedelta, date
+from math import sqrt
+from scipy.stats import t
+
+
+
+#Dictionary to map units and activity
+eventname_unit={
+    
+    'stepsperminute':'steps',
+    'kilo_calories':'calories',
+    'meters':'distance'
+    }
+
+#Include activity type that needs to be summed up
+activity_cumulative=['steps']
+
+#Dictionary to map analysisid by activity:
+    
+activity_analysisid={
+     'biking':1,
+     'steps':2,
+    'calories':3,
+    'distance':4
+     
+     }   
+
+dct_activity={
+        
+        'running' : {'no_activity': [0,0], 'short_run': [1, 15], 'medium_run' : [16, 30], 'long_run':[31, np.Inf]},
+
+'biking': {'no_activity':[0,0], 'short_bike': [1,30] ,'medium_bike': [31,60],'long_bike': [61,np.Inf]},
+
+'mindfulness': {'no_activity':[0,0], 'short_mindfulness': [1,5] ,'medium_mindfulness': [6,10],'long_mindfulness': [11,np.Inf]},
+ 
+'strengthtraining': {'no_activity':[0,0], 'short_strengthtraining': [11,20] ,'medium_strengthtraining': [21,50],'long_strengthtraining': [51,np.Inf]},
+
+    'Walking': {'no_activity':[0,0], 'short_walk': [11,20] ,'medium_walk': [21,50],'long_walk': [51,np.Inf]},
+    
+    #'steps':{'no_activity':[0,0], 'shortduration_steps': [1,600] ,'mediumduration_steps': [601,822],'longduration_steps': [823,np.Inf]}
+    'stepscount':{'no_activity':[0,0], 'low_steps_perday': [1,4300] ,'medium_steps_perday': [4301,10000],'high_steps_perday': [10301,np.Inf] }
+    
+    
+}
+
+
+
+
+def timestamp_modify(datastream):
+    if 'timestamp' in datastream.columns:
+        
+        if any("perminute" in s for s in datastream.unit.unique()):        
+            l=list(filter(lambda x: "perminute" in x, datastream.unit.unique()))
+            print(l)
+            datastream=datastream[datastream.unit==l[0]]
+ 
+            datastream['start_time']=datastream['timestamp']-pd.Timedelta('1min')
+            datastream.rename(columns={'timestamp':'end_time','individual_id':'user_id'},inplace=True)
+            
+            
+        else:
+            datastream['start_time']=datastream['timestamp']
+            datastream.rename(columns={'timestamp':'end_time','individual_id':'user_id'},inplace=True)
+
+            
+            
+        datastream=datastream[datastream.unit.notnull()]
+            
+        return datastream[['user_id','start_time','end_time','source', 'value', 'unit', 'confidence']]
+    
+    else:
+        datastream.rename(columns={'individual_id':'user_id'},inplace=True)
+        
+    datastream=datastream[datastream.unit.notnull()]
+    return datastream
+        
+
+
+def events_overlap(eventstream):
+    
+    eventstream=eventstream[(eventstream.event_name!='')&(eventstream.event_name.notnull())]
+
+    data=eventstream.sort_values(by=['start_time']).copy()
+
+    data["end_time_lag"] = data['end_time'].shift()
+    def merge(x):
+
+        if (x.start_time <=x.end_time_lag):
+            x.end_time = max(x.end_time, x.end_time_lag)
+
+        return x
+
+    res = pd.DataFrame([])
+    for k,grp in (data.groupby(['user_id','event_name','source'],as_index=False)):
+
+        res = pd.concat([grp.apply(merge, axis=1).groupby(['user_id','event_name','source'],as_index=False).apply(lambda e: e.assign(
+            grp=lambda d: (
+                ~(d["start_time"] <= (d["end_time"].shift()))
+                ).cumsum())), res])
+
+
+
+    data = res.groupby(['user_id','event_name','grp'],as_index=False).agg({"start_time": "min", "end_time": "max"
+                                                                                        ,'parameters': list,
+                                                                                        'source':';'.join})
+
+    def param_append(list1,total_duration):
+            dct={}
+            totalcaloriesburned=0
+            for k,v in enumerate(list1):
+                #v=v.replace("'",'"')
+                #v=json.loads(v)
+                dct['param'+str(k)]=v
+
+                if 'caloriesBurned' in v:
+                    totalcaloriesburned+=v['caloriesBurned']
+
+            if totalcaloriesburned!=0:
+                dct['totalcaloriesburned']=totalcaloriesburned
+
+            dct['duration']=np.int(total_duration*60000)
+
+            return dct
+
+    data.drop(columns='grp',inplace=True)
+
+
+    data['duration_minutes'] = data['end_time'] - data['start_time']
+    data['duration_minutes']=data['duration_minutes']/np.timedelta64(1,'m')
+
+
+    lst=[]
+    for i in range(data.shape[0]):
+        temp=param_append(data.iloc[i,data.columns.get_loc('parameters')],data.iloc[i,data.columns.get_loc('duration_minutes')])
+        lst.append(temp)
+
+
+    data['parameter2']=lst
+
+
+    data['parameter2'] = data['parameter2'].apply(lambda x: json.dumps(x))
+
+
+    data=data[['user_id', 'start_time', 'end_time', 'event_name', 'source','parameter2']].copy()
+
+    return data
+
+
+
+def getCategory(num, dct):
+    
+    for key in dct.keys():
+        if dct[key][0] <= num <= dct[key][1]:
+            return key
+
+
+def insights_generate(insight_activity,pivot_sleep):
+    current_activity=insight_activity #change here
+
+
+    stats_sleep=[]
+    stats_sleep = pivot_sleep.groupby(['user_id',current_activity])['sleep_duration'].agg(['mean', 'count', 'std'])
+    #display(stats_sleep)
+
+    ci95_hi = []
+    ci95_lo = []
+
+    for i in stats_sleep.index:
+        m, c, s = stats_sleep.loc[i]
+        ci95_hi.append(m + 1.96*s/math.sqrt(c))
+        ci95_lo.append(m - 1.96*s/math.sqrt(c))
+
+    stats_sleep['ci95_hi'] = ci95_hi
+    stats_sleep['ci95_lo'] = ci95_lo
+    #stats_sleep['event_name']='sleep'
+
+
+    alert=stats_sleep.reset_index().copy()
+
+
+    alert.rename(columns={current_activity:'event_summary'},inplace=True)
+    
+
+
+
+
+    temp2={}
+    ds = []
+
+    for user_id in alert.user_id.unique():
+        user_df = alert[alert.user_id==user_id]
+        event_summary = set(user_df.event_summary)
+        if 'no_activity' in event_summary:
+            activities = event_summary.copy()
+            activities.remove('no_activity')
+            for activity in activities:
+                noactivity_high = user_df[user_df.event_summary=='no_activity'].iloc[:,user_df.columns.get_loc('ci95_hi')].values[0]
+                noactivity_low = user_df[user_df.event_summary=='no_activity'].iloc[:,user_df.columns.get_loc('ci95_lo')].values[0]
+                noactivity_meansleep = user_df[user_df.event_summary=='no_activity'].iloc[:,user_df.columns.get_loc('mean')].values[0]
+
+                activity_high = user_df[user_df.event_summary==activity].iloc[:,user_df.columns.get_loc('ci95_hi')].values[0]
+                activity_low = user_df[user_df.event_summary==activity].iloc[:,user_df.columns.get_loc('ci95_lo')].values[0]
+                activity_meansleep=user_df[user_df.event_summary==activity].iloc[:,user_df.columns.get_loc('mean')].values[0]
+
+
+
+                noactivitycount = user_df[user_df.event_summary=='no_activity'].iloc[:,user_df.columns.get_loc('count')].values[0]
+                activitycount = user_df[user_df.event_summary==activity].iloc[:,user_df.columns.get_loc('count')].values[0]
+                deg = (noactivitycount + activitycount - 2) #deegrees of freedom
+
+                stdactivity = user_df[user_df.event_summary==activity].iloc[:,user_df.columns.get_loc('std')].values[0]
+                stdnoactivity = user_df[user_df.event_summary=='no_activity'].iloc[:,user_df.columns.get_loc('std')].values[0]
+                std_N1N2 = sqrt( ((activitycount - 1)*(stdactivity)**2 + (noactivitycount - 1)*(stdnoactivity)**2) / deg) #average standard deviations between groups.
+                diff_mean = abs(activity_meansleep-noactivity_meansleep)
+
+
+                temp2['user_id']=user_id
+                temp2['life_aspect']='sleep'
+                temp2['timestampadded']=strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                temp2['expirydate']=(datetime.datetime.now() + timedelta(days=7) ).strftime('%Y-%m-%d')
+                temp2['view']='No'
+
+                MoE = t.ppf(0.95, deg) * std_N1N2 * sqrt(1/(activitycount) + 1/(noactivitycount)) # margin of error 
+
+                upperci= np.round(diff_mean+MoE,2)
+                lowerci= np.round(diff_mean-MoE,2)
+
+
+
+                if (0<lowerci):
+
+                    msg = ''
+                    if activity_meansleep > noactivity_meansleep:
+
+                        msg = f"""{dct_activity[current_activity][activity][0]} to {dct_activity[current_activity][activity][1]} mins of {current_activity} increased your sleep by {int((activity_meansleep - noactivity_meansleep)*60)} mins"""
+
+                    else:
+
+                        msg = f"""{dct_activity[current_activity][activity][0]} to {dct_activity[current_activity][activity][1]} mins of {current_activity} reduced your sleep by {abs(int((activity_meansleep - noactivity_meansleep)*60))} mins"""
+
+                    if "inf" in msg:
+                        msg = "More than " + msg.replace("to inf", "")
+
+                    temp2['insighttext'] = msg
+                    
+                    temp2['impact']=np.where("increased" in msg,'positive','negative')
+                    
+                    
+
+                    ds.append(copy.deepcopy(temp2))
+
+
+                else:
+                    pass
+                
+            return ds
+        
+        
+        
+
+
+
